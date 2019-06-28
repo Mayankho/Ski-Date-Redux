6670a0cd426242272c14e7a346852caf5d60f7d4
Mayankho <yankhot96@gmail.com>
2019-06-27 23:03:09 -0400

    changed app and index

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
-import {createStore} from 'redux'
+import {createStore} from 'redux';
 import './App.css';
 
 
-function reducer(){
-  return{
-    user: {
-      name: 'Dustin'
-    }, 
-    movies: [
-      'Star Wars', 
-      'Lord of the Rings', 
-      'Harry Potter'
-    ],
-    todoList: [
-      {task: 'Learn Redux', id: 0, completed: false }
-    ],
-    moviesToWatch: 13
-  }
-}
+
 
 // Reducer is the same as having state
 
-const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
       <p>
-        This is react!
+        React is great!
       </p>
+      
+
      
     </div>
   );
